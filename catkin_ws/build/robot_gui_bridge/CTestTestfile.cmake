4bf6eacd3acfaadfd3358a1d35e4a2a05c6c2b96
onuchukwu <onuchukwu@onuchukwu.onuchukwu.virtualbox.org>
2023-03-28 00:48:34 +0100

    feat: communication between ros bridge and web using topics

---
+++ catkin_ws/build/robot_gui_bridge/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/onuchukwu/Documents/Projects/bridge_tut/catkin_ws/src/robot_gui_bridge
+# Build directory: /home/onuchukwu/Documents/Projects/bridge_tut/catkin_ws/build/robot_gui_bridge
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
